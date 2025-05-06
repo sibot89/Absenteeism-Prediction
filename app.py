2d5f66793f5672a3d9989835b672249388041e3d
Sima Baynaqi <simabaynaqi@gmail.com>
2025-05-06 19:22:36 +0330

    Add files via upload
    upload app.py file that is a python file implemented using absenteeism module for prediction and streamlit for creating a web page

---
+++ app.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+import pickle
+
+from absenteeism_module import *
+
+
+def Input_Output():
+    data = st.file_uploader("Please Upload Your File Here", type={"csv", "txt"})
+    if data is not None:
+        df = pd.read_csv(data)
+        st.write(df)
+        model = absenteeism_model('model', 'scaler')
+
+        model.load_and_clean_data('Absenteeism_new_data.csv')
+
+    result = ""
+    if st.button("Click here to Predict"):
+        result = model.predicted_outputs()
+        st.snow()
+        # st.balloons()     
+    
+    st.success('The output is as follows: ')
+    st.write(result)
+
+
+if __name__ ==  '__main__':
+    Input_Output()
